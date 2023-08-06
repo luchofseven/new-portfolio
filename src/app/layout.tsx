eb4259f30374b3b16ac9c19c8f0fad806a7d557e
Luciano Fernandez <luchofseven@gmail.com>
2023-08-05 22:53:13 -0300

    fixed meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,12 @@ import { arimo, dmSerifDisplay } from '@/fonts/fonts'
 import '../styles/styles.min.css'
 
 export const metadata: Metadata = {
-  title: 'Luciano Fernández - Portafolio Web'
+  title: 'Luciano Fernández - Portafolio Web',
+  description: 'Portafolio personal de Luciano Fernández, desarrollador web front-end',
+  applicationName: 'Luciano Fernández',
+  appleWebApp: { title: 'Luciano Fernández' },
+  twitter: { title: 'Luciano Fernández - Portafolio Web', images: 'https://portfolio-zeta-five-47.vercel.app/lf-logo.webp', card: 'summary_large_image', site: 'Luciano Fernández', creator: '@luchofseven', description: 'Portafolio personal de Luciano Fernández, desarrollador web front-end' },
+  openGraph: { title: 'Luciano Fernández - Portafolio Web', type: 'website', images: 'https://portfolio-zeta-five-47.vercel.app/lf-logo.webp', url: 'https://portfolio-zeta-five-47.vercel.app/', description: 'Portafolio personal de Luciano Fernández, desarrollador web front-end', locale: 'es_ES', siteName: 'Luciano Fernández' }
 }
 
 export default function RootLayout ({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import Footer from '@/components/Footer'
 import Hero from '@/components/Hero'
 import Navbar from '@/components/Navbar'
@@ -8,28 +7,6 @@ import AboutMe from '@/components/AboutMe'
 export default function Home (): JSX.Element {
   return (
   <>
-      <Head>
-        <title>Luciano Fernández - Portafolio Web</title>
-        <meta name='title' content='Luciano Fernández - Portafolio Web' />
-        <meta name='description' content='Portafolio personal de Luciano Fernández, desarrollador web front-end' />
-        <meta name='application-name' content='Luciano Fernández' />
-        <meta name='apple-mobile-web-app-title' content='Luciano Fernández' />
-        <meta property='og:title' content='Luciano Fernández - Portafolio Web' />
-        <meta property='og:type' content='website' />
-        <meta property='og:image' content='https://portfolio-zeta-five-47.vercel.app/lf-logo.webp' />
-        <meta property='og:url' content='https://portfolio-zeta-five-47.vercel.app/' />
-        <meta property='og:description' content='Portafolio personal de Luciano Fernández, desarrollador web front-end' />
-        <meta property='og:locale' content='es_ES' />
-        <meta property='og:site_name' content='Luciano Fernández' />
-        <meta name='twitter:card' content='summary_large_image' />
-        <meta name='twitter:site' content='@luchofseven' />
-        <meta name='twitter:creator' content='@luchofseven' />
-        <meta name='twitter:title' content='Luciano Fernández - Portafolio Web' />
-        <meta name='twitter:description' content='Portafolio personal de Luciano Fernández, desarrollador web front-end' />
-        <meta name='twitter:image' content='https://portfolio-zeta-five-47.vercel.app/lf-logo.webp' />
-        <meta name='twitter:domain' content='portfolio-zeta-five-47.vercel.app' />
-        <meta name='twitter:url' content='https://portfolio-zeta-five-47.vercel.app/' />
-      </Head>
       <Navbar />
       <main className="main-app">
         <Hero />
