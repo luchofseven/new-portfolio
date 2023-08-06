57790bd2ea9067cd412dfcfc61bd9df5bf8d4e02
Luciano Fernandez <luchofseven@gmail.com>
2023-08-05 23:49:50 -0300

    fixed meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,8 @@ export const metadata: Metadata = {
   description: 'Portafolio personal de Luciano Fernández, desarrollador web front-end',
   applicationName: 'Luciano Fernández',
   appleWebApp: { title: 'Luciano Fernández' },
-  twitter: { title: 'Luciano Fernández - Portafolio Web', images: 'https://portfolio-zeta-five-47.vercel.app/lf-logo.webp', card: 'summary_large_image', site: 'Luciano Fernández', creator: '@luchofseven', description: 'Portafolio personal de Luciano Fernández, desarrollador web front-end' },
-  openGraph: { title: 'Luciano Fernández - Portafolio Web', type: 'website', images: 'https://portfolio-zeta-five-47.vercel.app/lf-logo.webp', url: 'https://portfolio-zeta-five-47.vercel.app/', description: 'Portafolio personal de Luciano Fernández, desarrollador web front-end', locale: 'es_ES', siteName: 'Luciano Fernández' }
+  twitter: { title: 'Luciano Fernández - Portafolio Web', card: 'summary_large_image', site: 'Luciano Fernández', creator: '@luchofseven', images: 'https://portfolio-zeta-five-47.vercel.app/lf-logo.png', description: 'Portafolio personal de Luciano Fernández, desarrollador web front-end' },
+  openGraph: { title: 'Luciano Fernández - Portafolio Web', type: 'website', images: [{ url: 'https://portfolio-zeta-five-47.vercel.app/lf-logo.png' }], url: 'https://portfolio-zeta-five-47.vercel.app/', description: 'Portafolio personal de Luciano Fernández, desarrollador web front-end', locale: 'es_ES', siteName: 'Luciano Fernández' }
 }
 
 export default function RootLayout ({
