8bb4dc52bffd399039dc58699455e45af53debee
Luciano Fernandez <luchofseven@gmail.com>
2023-08-09 15:46:02 -0300

    updated projects info and improved some general styles

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -10,49 +10,49 @@ export default function Projects (): JSX.Element {
           <Card
             img='/project-municipalidad-vera.webp'
             title='Muncipalidad de Vera'
-            description='Proyecto del sitio web de la Municipalidad de Vera. Se realizó un rediseño de la página actual. Sitio construído con React y TypeScript.'
+            description='Sitio web de la Municipalidad de Vera. En el cual se realizó un rediseño de la página actual. Construído con React y TypeScript.'
             urlDeploy='https://municipalidad-vera.vercel.app/'
             urlRepo='https://github.com/luchofseven/municipalidad-vera'
           />
           <Card
           img='/project-tesla-landing.webp'
             title='Tesla Landing Clone'
-            description='Proyecto de clonación de la landing de Tesla. Sitio construído con Astro y Tailwind.'
+            description='Clonación del sitio web de Tesla. En base a un stream de @midudev. Construído con Astro y Tailwind.'
             urlDeploy='https://tesla-clone-henna-nine.vercel.app/'
             urlRepo='https://github.com/luchofseven/tesla-clone'
           />
           <Card
           img='/project-todo-app.webp'
             title='TO-DO App'
-            description='Proyecto de "Lista de tareas" en el cual se pueden agregar, editar o eliminar las mismas. En este caso se utilizó Next.Js y Tailwind.'
+            description='"Lista de tareas", en el cual se pueden agregar, editar o eliminar las mismas. Construído con Next.Js y Tailwind.'
             urlDeploy='https://todo-app-next-woad.vercel.app/'
             urlRepo='https://github.com/luchofseven/crud-nextjs-tailwind'
           />
           <Card
           img='/project-search-movies-app.webp'
             title='Movies Search App'
-            description='Proyecto para buscar y listar películas y series. El mismo está realizado con React, haciendo uso de hooks y también la implementación de un debounce, para que al escribir la búsqueda, la petición se haga de forma automática luego de haber transcurrido algunos segundos.'
+            description='Sitio web para buscar y listar películas y series. El mismo está realizado con React, haciendo uso de hooks y también la implementación de un debounce, para que al escribir la búsqueda, la petición se haga de forma automática luego de haber transcurrido algunos segundos.'
             urlDeploy='https://movies-search-app-phi.vercel.app/'
             urlRepo='https://github.com/luchofseven/movies-search-app'
           />
           <Card
           img='/project-bonsai.webp'
             title='Bonsai Clone'
-            description='Proyecto de clonación de la landing de Bonsai. El sitio está construído con React y Sass.'
+            description='Clonación del sitio web de Bonsai, en el cual se puede elegir y simular el cambio de planes y precios. Proyecto obtenido de "Front-end Practice". El sitio está construído con React y Sass.'
             urlDeploy='https://bonsai-proyecto-react.vercel.app/'
             urlRepo='https://github.com/luchofseven/bonsai-proyecto-react'
           />
           <Card
           img='/project-mohana-indumentaria.webp'
             title='Mohana Indumentaria'
-            description='Proyecto de Mohana Indumentaria. Sitio construído con React y Firebase.'
+            description='Proyecto a modo de simular un comercio electrónico, dónde se pueden ver los productos disponibles, su stock, sus talles, así como también poder añadirlos al carrito de compras. Sitio construído con React y Firebase.'
             urlDeploy='https://mohana-proyecto-react.vercel.app/'
             urlRepo='https://github.com/luchofseven/mohana-proyecto-react'
           />
           <Card
           img='/project-mohana.webp'
             title='Mohana Indumentaria'
-            description='Proyecto de catálogo para Mohana Indumentaria. Utilizando Firestore para alojar los productos. Sitio construído con JavaScript y Firebase.'
+            description='Catálogo de Mohana Indumentaria. Utilizando Firestore para alojar los productos. Sitio construído con JavaScript y Firebase.'
             urlDeploy='https://luchofseven.github.io/mohana-indumentaria/'
             urlRepo='https://github.com/luchofseven/mohana-indumentaria'
           />
