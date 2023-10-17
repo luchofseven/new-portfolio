08f9058b342290fbc3370f11ed1eaf7f2767df0b
Luciano Fernandez <luchofseven@gmail.com>
2023-10-17 14:34:54 -0300

    added new  og:image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,13 +4,13 @@ import '../styles/styles.min.css'
 
 export const metadata: Metadata = {
   title: 'Luciano Fernández - Portafolio Web',
-  description: 'Portafolio personal de Luciano Fernández, desarrollador web front-end.',
+  description: 'Mi portafolio personal, donde podrás encontrar mis proyectos y contactarte conmigo.',
   applicationName: 'Luciano Fernández',
   icons: '/lf-logo.png',
   themeColor: '#ec5cff',
   appleWebApp: { title: 'Luciano Fernández' },
-  twitter: { title: 'Luciano Fernández - Portafolio Web', card: 'summary_large_image', site: 'Luciano Fernández', creator: '@luchofseven', images: '/lf-logo.png', description: 'Portafolio personal de Luciano Fernández, desarrollador web front-end' },
-  openGraph: { title: 'Luciano Fernández - Portafolio Web', type: 'website', images: [{ url: '/lf-logo.png' }], url: 'https://luchofseven.com.ar', description: 'Portafolio personal de Luciano Fernández, desarrollador web front-end', locale: 'es_ES', siteName: 'Luciano Fernández' }
+  twitter: { title: 'Luciano Fernández - Portafolio Web', card: 'summary_large_image', site: 'Luciano Fernández', creator: '@luchofseven', images: '/og-logo.png', description: 'Portafolio personal de Luciano Fernández, desarrollador web front-end' },
+  openGraph: { title: 'Luciano Fernández - Portafolio Web', type: 'website', images: [{ url: '/og-logo.png' }], url: 'https://luchofseven.com.ar', description: 'Portafolio personal de Luciano Fernández, desarrollador web front-end', locale: 'es_ES', siteName: 'Luciano Fernández' }
 }
 
 export default function RootLayout ({
