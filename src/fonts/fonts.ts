a97fff831aa34ea723df6496769f11876c840b82
Luciano Fernandez <luchofseven@gmail.com>
2025-05-19 18:36:22 -0300

    fix

--- src/fonts/fonts.ts
+++ src/fonts/fonts.ts
@@ -4,16 +4,19 @@ export const poppins = Poppins({
   subsets: ['latin'],
   weight: '400',
   variable: '--font-poppins',
+  display: 'swap',
 });
 
 export const dmSerifDisplay = DM_Serif_Display({
   subsets: ['latin'],
   weight: '400',
   variable: '--font-dmSerifDisplay',
+  display: 'swap',
 });
 
 export const lilitaOne = Lilita_One({
   subsets: ['latin'],
   weight: '400',
   variable: '--font-lilitaOne',
+  display: 'swap',
 });
